de069421386321240410150a045bcf80dceb37f4
Islam Tarek <islamtarek0550@gmail.com>
2023-05-06 15:23:30 +0300

    Add Atmega32 Registers File

---
+++ 01-SRC/02-Atmega32_Registers/atmega32_registers.h
@@ -0,0 +1,15 @@
+/*****************************************************************************
+ * @Author                : Islam Tarek<islamtarek0550@gmail.com>            *
+ * @CreatedDate           : 2023-05-06 15:22:08                              *
+ * @LastEditors           : Islam Tarek<islamtarek0550@gmail.com>            *
+ * @LastEditDate          : 2023-05-06 15:22:56                              *
+ * @FilePath              : atmega32_registers.h                             *
+ ****************************************************************************/
+
+#ifndef ATMEGA32_REG_H_
+#define ATMEGA32_REG_H_
+
+
+
+
+#endif
